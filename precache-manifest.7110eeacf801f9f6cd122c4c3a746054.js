da7963a11dc76a1fae9a24fa2dd0f30fc451926c
Yakraj Pariyar <59973173+yakraj@users.noreply.github.com>
2020-07-28 21:03:47 +0530

    Add files via upload

---
+++ precache-manifest.7110eeacf801f9f6cd122c4c3a746054.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bb827a816f36f542fe19fb165e3558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc6ec18d1160304af528",
+    "url": "/static/css/main.821b465c.chunk.css"
+  },
+  {
+    "revision": "b0559c305b5b22ac10da",
+    "url": "/static/js/2.03f54393.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.03f54393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6ec18d1160304af528",
+    "url": "/static/js/main.57eb2c2a.chunk.js"
+  },
+  {
+    "revision": "ae8125c96a26dc32b6b2",
+    "url": "/static/js/runtime-main.0e62ee71.js"
+  },
+  {
+    "revision": "c90dac0ca0dbd33367ffd447bae74589",
+    "url": "/static/media/account.c90dac0c.svg"
+  },
+  {
+    "revision": "f1e47d9fb51224cf0b0ccbd263700405",
+    "url": "/static/media/catogery.f1e47d9f.svg"
+  },
+  {
+    "revision": "4ee672b165f8b06ca3ddc2bf155397f0",
+    "url": "/static/media/chat.4ee672b1.svg"
+  },
+  {
+    "revision": "5672e408b9b78c646941ca7ccdcf947a",
+    "url": "/static/media/home.5672e408.svg"
+  },
+  {
+    "revision": "f9fc24248b4699fa0155068b6f805418",
+    "url": "/static/media/iphone.f9fc2424.png"
+  },
+  {
+    "revision": "2d68ecfd6dbb0ec7eaf36d745ca130dc",
+    "url": "/static/media/location.2d68ecfd.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "276721d95d8cceef072b9a6a4b078ddf",
+    "url": "/static/media/profile.276721d9.jpg"
+  },
+  {
+    "revision": "3f965605e6dac3e081e6c81f2fff3fc2",
+    "url": "/static/media/sell.3f965605.svg"
+  }
+]);
